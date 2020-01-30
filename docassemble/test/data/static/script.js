e09b0304778c4e86cd424ae9a65a97f62df8df52
Izaak Baker <izaak_baker@alumni.brown.edu>
2020-01-30 00:19:22 -0500

    test console log

--- docassemble/test/data/static/script.js
+++ docassemble/test/data/static/script.js
@@ -1,6 +1,6 @@
 function showAlert(interview_data) {
+    console.log(interview_data);
     var favorite_color = interview_data['favorite_color'];
     var favorite_city = interview_data['favorite_city'];
-    alert("Here's some custom JS!");
     alert("Your favorite color is " + favorite_color + " and your favorite city is " + favorite_city);
 }
