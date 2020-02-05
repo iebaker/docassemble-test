adf3875a1f4913d753ff55acb95bd70fb6cd41f5
Izaak Baker <izaak_baker@alumni.brown.edu>
2020-02-04 21:25:56 -0500

    remove local var

--- docassemble/test/data/static/script.js
+++ docassemble/test/data/static/script.js
@@ -5,9 +5,8 @@ function showAlert(interview_data) {
     alert("Your favorite color is " + favorite_color + " and your favorite city is " + favorite_city);
 }
 
-var map;
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
+    var map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -34.397, lng: 150.644},
         zoom: 8
     });
