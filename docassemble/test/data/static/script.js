cda1b86b45245e5de03e76ce6d0260d8d7deb15d
Izaak Baker <izaak_baker@alumni.brown.edu>
2020-02-05 09:47:14 -0500

    more ambitious

--- docassemble/test/data/static/script.js
+++ docassemble/test/data/static/script.js
@@ -5,9 +5,34 @@ function showAlert(interview_data) {
     alert("Your favorite color is " + favorite_color + " and your favorite city is " + favorite_city);
 }
 
+var map;
+var placesService;
+
 function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
+    var mumbai = new google.maps.LatLng(19.0760, 72.8777);
+    map = new google.maps.Map(document.getElementById('map'), { center: mumbai, zoom: 8 });
+    placesService = new google.maps.places.PlacesService(map);
+}
+
+function searchAndAddMarker(query) {
+    var mumbaiLocationBias = { radius: 80000, center: mumbai };
+    var request = {
+        query: query,
+        fields: ['name', 'geometry'],
+        locationBias: mumbaiLocationBias
+    };
+    placesService.findPlaceFromQuery(request, function(results, status) {
+        console.log(results);
+        console.log(status);
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+                var marker = new google.maps.Marker({
+                    position: results[i].geometry.location,
+                    title: results[i].name
+                });
+                marker.setMap(map);
+            }
+            map.setCenter(results[0].geometry.location);
+        }
     });
 }
