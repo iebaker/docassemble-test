e49da9fc6d502279097747d3f55a5a6c7bb579d7
Izaak Baker <izaak_baker@alumni.brown.edu>
2020-02-05 10:18:38 -0500

    up to version 7

--- docassemble/test/data/static/script.js
+++ docassemble/test/data/static/script.js
@@ -6,26 +6,35 @@ function showAlert(interview_data) {
 }
 
 function initMap(query) {
-    var mumbai = new google.maps.LatLng(19.0760, 72.8777);
-    var map = new google.maps.Map(document.getElementById('map'), { center: mumbai, zoom: 8 });
-    var placesService = new google.maps.places.PlacesService(map);var mumbaiLocationBias = { radius: 80000, center: mumbai };
-    var request = {
-        query: query,
-        fields: ['name', 'geometry'],
-        locationBias: mumbaiLocationBias
-    };
-    placesService.findPlaceFromQuery(request, function(results, status) {
-        console.log(results);
-        console.log(status);
-        if (status == google.maps.places.PlacesServiceStatus.OK) {
-            for (var i = 0; i < results.length; i++) {
-                var marker = new google.maps.Marker({
-                    position: results[i].geometry.location,
-                    title: results[i].name
-                });
-                marker.setMap(map);
-            }
-            map.setCenter(results[0].geometry.location);
+    get_interview_variables(function(data) {
+        console.log(data);
+        if (data.success) {
+            var variables = data.variables;
+            var mumbai = new google.maps.LatLng(19.0760, 72.8777);
+            var map = new google.maps.Map(document.getElementById('map'), { center: mumbai, zoom: 8 });
+            var placesService = new google.maps.places.PlacesService(map);var mumbaiLocationBias = { radius: 80000, center: mumbai };
+           
+            var request = {
+                query: variables['location_query_string'],
+                fields: ['name', 'geometry'],
+                locationBias: mumbaiLocationBias
+            };
+            
+            placesService.findPlaceFromQuery(request, function(results, status) {
+                console.log(results);
+                console.log(status);
+                if (status == google.maps.places.PlacesServiceStatus.OK) {
+                    for (var i = 0; i < results.length; i++) {
+                        var marker = new google.maps.Marker({
+                            position: results[i].geometry.location,
+                            title: results[i].name
+                        });
+                        marker.setMap(map);
+                    }
+                    map.setCenter(results[0].geometry.location);
+                }
+            });
         }
-    });
+    })
+
 }
